17dae31b28ee95e8187a6f0ca90467eba6e8eea6
beejay321 <jibodu.busola@gmail.com>
2021-11-18 20:56:48 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Home />
+      </Router>
+
+      {/* <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
