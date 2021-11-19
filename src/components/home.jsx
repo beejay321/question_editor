08eac5cd710c5176914f4137df64d767c0adfefe
beejay321 <jibodu.busola@gmail.com>
2021-11-19 23:41:15 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,14 +4,64 @@ import Rows from "./row";
 import Columns from "./columns";
 
 const Home = () => {
-  const [rows, setRows] = useState({
-    Col1: "",
-    Col2: "",
-  });
+  const [rows, setRows] = useState([
+    {
+      image1: "http://placehold.it/40",
+      title1: "row1",
+      Col1: "",
+      //   Col2: "",
+      //   Col3: "",
+    },
+    {
+      image1: "http://placehold.it/40",
+      title1: "row1",
+      Col1: "",
+      //   Col2: "",
+      //   Col3: "",
+    },
+  ]);
+
+  const [cols, setCols] = useState([
+    {
+      image1: "http://placehold.it/40",
+      title1: "Col1",
+      Row1: "",
+    },
+  ]);
+
+  const [no, setNo] = useState(false);
+
+  const handleChange = (index, event) => {
+    const values = [...rows];
+    values[index][event.target.name] = event.target.value;
+    setRows(values);
+  };
 
   const addRow = () => {
-   setRows([...rows, {Col1: "", Col2: ""}])
+    console.log("me");
+    setRows([
+      ...rows,
+      {
+        image1: "http://placehold.it/40",
+        title1: "row1",
+        Col1: "",
+        Col2: "",
+        Col3: "",
+      },
+    ]);
+  };
+  const addCol = () => {
+    console.log("addCol");
+    const newRows = rows.map((row) => ({...row, newCol :""}));    
 
+    setRows(newRows);
+    console.log(newRows);
+    
+    // const columns = rows[0].getElementsByTagName("td").length;
+    // if (!pos && pos !== 0) {
+    //   pos = columns;
+    // }
+  };
   return (
     <>
       <div className="d-flex justify-content-center pt-5">
@@ -29,21 +79,59 @@ const Home = () => {
               </h5>
             </div>
             <Table>
-              <Columns />
-              <Rows />
-
+              {/* <Columns /> */}
+              {/* <Rows /> */}
               <tbody>
                 <tr id="eachRow">
+                  <td></td>
                   <td>
-                      <input>
-                      Option 1</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
+                    <div className="d-grid ">
+                      <img className=" " src="http://placehold.it/40" alt="Dummy Image" />
+                      <span contenteditable="true">
+                        <em>Col 1</em>
+                      </span>
+                    </div>
+                  </td>
+
+                  <td>
+                    <Button onClick={addCol}>+</Button>
+                  </td>
+                </tr>
+                {rows.map((row, index) => (
+                  <tr id="eachRow" key={index}>
+                    <td>
+                      <div className="d-flex justify-content-start gap-2 ">
+                        <img className="d-flex justify-content-center" src="http://placehold.it/40" alt="Dummy Image" />
+                        <span contenteditable="true" className="d-flex align-items-center">
+                          <em>{row.title1}</em>
+                        </span>
+                      </div>
+                    </td>
+                    <td>
+                      <input type="radio" id="huey" name="col1" defaultValue={row.Col1} />
+                      {/* <input type="radio" id="huey" name="col1" defaultValue={row.Col1} checked={true} onChange={(event) => handleChange(index, event)} /> */}
+                    </td>
+                    {/* <td>
+                      <input type="radio" id="huey" name="col2" defaultValue={row.Col2} />
+                    </td>
+                    <td>
+                      <input type="radio" id="huey" name="col3" defaultValue={row.Col3} />
+                    </td>
+                    <td>
+                      <input type="radio" id="huey" name="col4" defaultValue={row.Col3} />
+                    </td>
+                    <td>
+                      <input type="radio" id="huey" name="col5" defaultValue={row.Col3} />
+                    </td> */}
+                  </tr>
+                ))}
+                <tr id="eachRow">
+                  <td>
+                    <Button onClick={addRow}>+</Button>
+                  </td>
                 </tr>
               </tbody>
             </Table>
-            <Button onClick={addRow}>+</Button>
           </Col>
           <Col className="d-flex justify-content-center pt-5">
             <h3>Question Summary View</h3>
