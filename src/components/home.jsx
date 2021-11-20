6241710a06b094fd28c73a1d699b338984127a4b
beejay321 <jibodu.busola@gmail.com>
2021-11-20 23:40:40 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,67 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, Row, Col, Table, Button } from "react-bootstrap";
 import Rows from "./row";
 import Columns from "./columns";
-
+import OneRow from "./OneRow";
 const Home = () => {
-  const [rows, setRows] = useState([
-    {
-      image1: "http://placehold.it/40",
-      title1: "row1",
-      Col1: "",
-      //   Col2: "",
-      //   Col3: "",
-    },
-    {
-      image1: "http://placehold.it/40",
-      title1: "row1",
-      Col1: "",
-      //   Col2: "",
-      //   Col3: "",
-    },
-  ]);
-
-  const [cols, setCols] = useState([
-    {
-      image1: "http://placehold.it/40",
-      title1: "Col1",
-      Row1: "",
-    },
-  ]);
-
-  const [no, setNo] = useState(false);
-
-  const handleChange = (index, event) => {
-    const values = [...rows];
-    values[index][event.target.name] = event.target.value;
-    setRows(values);
+    
+  const itemObj = {
+    id: 0,
+    title: "something",
   };
+
+  const items = [itemObj];
+  const itemsArr = [items, items];
+
+  const [oneRow, setOneRow] = useState(itemsArr);
+  const [oneCol, setOneCol] = useState(items);
 
   const addRow = () => {
-    console.log("me");
-    setRows([
-      ...rows,
-      {
-        image1: "http://placehold.it/40",
-        title1: "row1",
-        Col1: "",
-        Col2: "",
-        Col3: "",
-      },
-    ]);
+    setOneRow([...oneRow, oneRow[oneRow.length - 1]]);
   };
-  const addCol = () => {
-    console.log("addCol");
-    const newRows = rows.map((row) => ({...row, newCol :""}));    
 
-    setRows(newRows);
-    console.log(newRows);
-    
-    // const columns = rows[0].getElementsByTagName("td").length;
-    // if (!pos && pos !== 0) {
-    //   pos = columns;
-    // }
+  const addCol = () => {
+    setOneRow([...oneRow.map((x) => [...x, itemObj])]);
+    console.log(oneRow);
   };
+
   return (
     <>
       <div className="d-flex justify-content-center pt-5">
@@ -74,64 +37,31 @@ const Home = () => {
               <h3>Question Edition View</h3>
             </div>
             <div className="pt-4">
-              <h5>
+              <h5 contenteditable="true" >
                 <em>Title Of Question</em>
               </h5>
             </div>
-            <Table>
-              {/* <Columns /> */}
-              {/* <Rows /> */}
-              <tbody>
-                <tr id="eachRow">
-                  <td></td>
-                  <td>
-                    <div className="d-grid ">
-                      <img className=" " src="http://placehold.it/40" alt="Dummy Image" />
-                      <span contenteditable="true">
-                        <em>Col 1</em>
-                      </span>
-                    </div>
-                  </td>
-
+            <div className="d-flex ">
+              <Table>
+                <tbody>
+                  {oneRow.map((row) => (
+                    <OneRow row={row} />
+                  ))}
+                  <tr>
+                    <td>
+                      <Button onClick={addRow}>+</Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <div>
+                <span>
                   <td>
                     <Button onClick={addCol}>+</Button>
-                  </td>
-                </tr>
-                {rows.map((row, index) => (
-                  <tr id="eachRow" key={index}>
-                    <td>
-                      <div className="d-flex justify-content-start gap-2 ">
-                        <img className="d-flex justify-content-center" src="http://placehold.it/40" alt="Dummy Image" />
-                        <span contenteditable="true" className="d-flex align-items-center">
-                          <em>{row.title1}</em>
-                        </span>
-                      </div>
-                    </td>
-                    <td>
-                      <input type="radio" id="huey" name="col1" defaultValue={row.Col1} />
-                      {/* <input type="radio" id="huey" name="col1" defaultValue={row.Col1} checked={true} onChange={(event) => handleChange(index, event)} /> */}
-                    </td>
-                    {/* <td>
-                      <input type="radio" id="huey" name="col2" defaultValue={row.Col2} />
-                    </td>
-                    <td>
-                      <input type="radio" id="huey" name="col3" defaultValue={row.Col3} />
-                    </td>
-                    <td>
-                      <input type="radio" id="huey" name="col4" defaultValue={row.Col3} />
-                    </td>
-                    <td>
-                      <input type="radio" id="huey" name="col5" defaultValue={row.Col3} />
-                    </td> */}
-                  </tr>
-                ))}
-                <tr id="eachRow">
-                  <td>
-                    <Button onClick={addRow}>+</Button>
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
+                  </td>{" "}
+                </span>
+              </div>
+            </div>
           </Col>
           <Col className="d-flex justify-content-center pt-5">
             <h3>Question Summary View</h3>
