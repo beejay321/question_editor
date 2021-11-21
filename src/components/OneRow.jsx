7d4615d83ef87f58fc97416ec5940ef62fffa895
beejay321 <jibodu.busola@gmail.com>
2021-11-21 15:27:48 +0100

    Update OneRow.jsx

--- src/components/OneRow.jsx
+++ src/components/OneRow.jsx
@@ -1,15 +1,37 @@
-import React from "react";
-import { Container, Row, Col, Table, Form } from "react-bootstrap";
+import React, { useState, useRef } from "react";
+import { Container, Row, Col, Table, Form, Button } from "react-bootstrap";
+import ImageModal from "./ImageModal";
 import OneColumn from "./OneColumn";
 
-const OneRow = (props) => {
+const OneRow = ({ index, row }) => {
   return (
     <>
-      <tr>
-        {props.row.map((col) => (
-          <OneColumn col={col} />
-        ))}
-      </tr>
+      {index === 0 ? (
+        <tr key={index}>
+          <div></div>
+          {row.map((col, i) => (
+            <>
+              <OneColumn col={col} i={i} rowIndex={index} />
+            </>
+          ))}
+        </tr>
+      ) : (
+        <tr key={index}>
+          <div className="d-flex justify-content-start gap-2 ">
+            {/* <img className=" " onClick={addRowImage} src={rowImage} alt="rowImage" /> */}
+            <ImageModal index={index} />
+
+            <span contenteditable="true">
+              <em>row {index}</em>
+            </span>
+          </div>
+          {row.map((col, i) => (
+            <>
+              <OneColumn col={col} i={i} rowIndex={index} />
+            </>
+          ))}
+        </tr>
+      )}
     </>
   );
 };
