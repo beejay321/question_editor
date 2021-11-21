875373bf6774b471ec20e5865573169923a35812
beejay321 <jibodu.busola@gmail.com>
2021-11-21 18:12:27 +0100

    Update OneRow.jsx

--- src/components/OneRow.jsx
+++ src/components/OneRow.jsx
@@ -1,9 +1,30 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Table, Form, Button } from "react-bootstrap";
 import ImageModal from "./ImageModal";
 import OneColumn from "./OneColumn";
+import { connect, useSelector, useDispatch } from "react-redux";
+import { getNoOfColumns } from "../redux/action";
 
 const OneRow = ({ index, row }) => {
+  const number = useSelector((state) => state.lengthOfRows);
+  const dispatch = useDispatch();
+  const [colCount, setColCount] = useState(0);
+  const [rowLabel, setRowLabel] = useState(`row ${index}`);
+
+  useEffect(() => {
+    console.log(rowLabel);
+    dispatch(getNoOfColumns(row));
+  }, [row]);
+
+  // useEffect(() => {
+  //   const rowLabelLength = () => {
+  //     console.log("rowLabelLength");
+  //     console.log(row.length);
+  //     setColCount(row.length);
+  //   };
+  //   rowLabelLength();
+  // }, [row]);
+
   return (
     <>
       {index === 0 ? (
@@ -20,9 +41,9 @@ const OneRow = ({ index, row }) => {
           <div className="d-flex justify-content-start gap-2 ">
             {/* <img className=" " onClick={addRowImage} src={rowImage} alt="rowImage" /> */}
             <ImageModal index={index} />
-
+            <input type="text" defaultValue={rowLabel} />
             <span contenteditable="true">
-              <em>row {index}</em>
+              <em>{rowLabel}</em>
             </span>
           </div>
           {row.map((col, i) => (
@@ -37,3 +58,4 @@ const OneRow = ({ index, row }) => {
 };
 
 export default OneRow;
+// export default connect(mapStateToProps, mapDispatchToProps)(OneRow);
