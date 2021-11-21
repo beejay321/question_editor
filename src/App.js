68161b05de52cb3e3d91cc74a11b4ac51b13ff43
beejay321 <jibodu.busola@gmail.com>
2021-11-21 15:27:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/home";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -10,17 +10,6 @@ function App() {
       <Router>
         <Home />
       </Router>
-
-      {/* <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div> */}
     </>
   );
 }
