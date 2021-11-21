61d9390e7a13c7ee7a5bc3b200b0a31266359c92
beejay321 <jibodu.busola@gmail.com>
2021-11-21 23:26:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 
 function App() {
