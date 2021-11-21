3c05515e8d148c55e72169b5a8947f96bfd7b076
beejay321 <jibodu.busola@gmail.com>
2021-11-21 15:27:58 +0100

    Create SummaryView.jsx

---
+++ src/components/SummaryView.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Container, Row, Col, Table, Button } from "react-bootstrap";
+
+const SummaryView = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-center pt-5">
+        <h1>This is the summary editor</h1>
+      </div>
+      <Container className=" "></Container>
+    </>
+  );
+};
+
+export default SummaryView;
