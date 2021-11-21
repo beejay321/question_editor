0479f5b7db2499a7c5be8bfbdfc671670a65d848
beejay321 <jibodu.busola@gmail.com>
2021-11-21 23:27:10 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Container, Row, Col, Table, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import EditionView from "./EditionView";
 import SummaryView from "./SummaryView";
 
@@ -8,12 +7,16 @@ const Home = () => {
     <>
       <Container className=" ">
         <Row>
-          <Col xs={12} md={6} className="d-grid justify-content-center pt-5">
-            <h3 className="d-flex justify-content-center">Question Edition View</h3>
+          <Col sm={12} lg={7} className="d-grid justify-content-start px-3  pt-5">
+            <h3 className="d-flex justify-content-start px-3">Question Edition View</h3>
             <EditionView />
           </Col>
-          <Col xs={12} md={6} className="d-grid justify-content-center pt-5">
-            <h3 className="d-flex justify-content-center">Question Summary View</h3>
+          <Col sm={0} lg={1}>
+            <div class="vertical"></div>
+          </Col>
+
+          <Col sm={12} lg={3} className="d-grid justify-content-start px-3 pt-5">
+            <h3 className="d-flex justify-content-start px-3">Question Summary View</h3>
             <SummaryView />
           </Col>
         </Row>
