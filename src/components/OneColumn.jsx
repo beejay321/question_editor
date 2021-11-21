0be7658509200b8cc1b7bafd5c0fd53b4f9f799b
beejay321 <jibodu.busola@gmail.com>
2021-11-21 15:27:45 +0100

    Update OneColumn.jsx

--- src/components/OneColumn.jsx
+++ src/components/OneColumn.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import { Container, Row, Col, Table, Form } from "react-bootstrap";
+import ImageModal from "./ImageModal";
+// import { Container, Row, Col, Table, Form } from "react-bootstrap";
 
-const OneColumn = ({ col }) => {
+const OneColumn = ({ col, i, rowIndex }) => {
   return (
     <>
-      <td>{col.title}</td>
+      {col.type === "image" ? (
+        <td>
+          <div className="d-grid justify-content-start gap-2 ">
+            {/* <img className="d-flex justify-content-start" src="http://placehold.it/40" alt="Dummy Image" /> */}
+            <ImageModal />
+            <span className="d-flex justify-content-start">
+              <span contentEditable="true">
+                <em>Col {i + 1}</em>
+              </span>
+            </span>
+          </div>
+        </td>
+      ) : (
+        <td>
+          <input type={col.type} id={col.id} name={rowIndex} defaultValue={col.value} />
+          {/* <input type={col.type} id={col.id} name={i} defaultValue={col.value} /> //  if the selected buttons were to have the same name vertically*/}
+        </td>
+      )}
     </>
   );
 };
