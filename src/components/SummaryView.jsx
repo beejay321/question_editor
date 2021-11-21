60bed5838220a5cc8041fbbf0a9a666d0efd2f73
beejay321 <jibodu.busola@gmail.com>
2021-11-21 23:26:57 +0100

    Update SummaryView.jsx

--- src/components/SummaryView.jsx
+++ src/components/SummaryView.jsx
@@ -1,24 +1,21 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Container, Row, Col, Table, Button } from "react-bootstrap";
-import { connect, useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { Container  } from "react-bootstrap";
+import {  useSelector } from "react-redux";
 
-const SummaryView = () => {
-  const [numRow] = useState("");
-  const [numCol, setNumCol] = useState("");
+const SummaryView = () => {  
   const [numImages, setNumImages] = useState("");
   const [rowLabel, setRowLabel] = useState("");
   const [colLabel, setColLabel] = useState("");
   const number = useSelector((state) => state);
 
-
   return (
     <>
       <Container className=" ">
-        <p>Number Of Rows : {number.lengthOfRows}</p>
-        <p>Number Of Columns : {number.lengthOfCols}</p>
-        <p>Number Of images Uploaded :</p>
-        <p>Longest row label :</p>
-        <p>Longest column label : </p>
+        <p className = "summaryText py-1">Number Of Rows : {number.lengthOfRows}</p>
+        <p className = "summaryText py-1">Number Of Columns : {number.lengthOfCols}</p>
+        <p className = "summaryText py-1">Number Of images Uploaded :</p>
+        <p className = "summaryText py-1">Longest row label :</p>
+        <p className = "summaryText py-1">Longest column label : </p>
       </Container>
     </>
   );
