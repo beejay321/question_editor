f491795b0039fac8ad7e7f18a09830cf7c1db34a
beejay321 <jibodu.busola@gmail.com>
2021-11-21 18:12:34 +0100

    Update SummaryView.jsx

--- src/components/SummaryView.jsx
+++ src/components/SummaryView.jsx
@@ -1,13 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Container, Row, Col, Table, Button } from "react-bootstrap";
+import { connect, useSelector, useDispatch } from "react-redux";
 
 const SummaryView = () => {
+  const [numRow] = useState("");
+  const [numCol, setNumCol] = useState("");
+  const [numImages, setNumImages] = useState("");
+  const [rowLabel, setRowLabel] = useState("");
+  const [colLabel, setColLabel] = useState("");
+  const number = useSelector((state) => state);
+
+
   return (
     <>
-      <div className="d-flex justify-content-center pt-5">
-        <h1>This is the summary editor</h1>
-      </div>
-      <Container className=" "></Container>
+      <Container className=" ">
+        <p>Number Of Rows : {number.lengthOfRows}</p>
+        <p>Number Of Columns : {number.lengthOfCols}</p>
+        <p>Number Of images Uploaded :</p>
+        <p>Longest row label :</p>
+        <p>Longest column label : </p>
+      </Container>
     </>
   );
 };
