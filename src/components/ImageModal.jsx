fb99c472bc8780fbdfd99cb534eb2b1292345fc0
beejay321 <jibodu.busola@gmail.com>
2021-11-21 20:51:22 +0100

    Update ImageModal.jsx

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { Modal, Button, Image, Table, Form } from "react-bootstrap";
+import { Modal, Button, Image, Spinner, Form } from "react-bootstrap";
 
 const ImageModal = ({ index }) => {
   const [show, setShow] = useState(false);
@@ -7,18 +7,20 @@ const ImageModal = ({ index }) => {
   const [rowImage, setRowImage] = useState("http://placehold.it/40");
   const [selectedImage, setSelectedImage] = useState("");
   const [selectedRow, setSelectedRow] = useState("");
+  const [imageLoading, setImageLoading] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const ADDRESS = "http://localhost:3255/rows";
+  // const ADDRESS = "https://question-editorr.herokuapp.com/rows";
 
   const addRowImage = async (e, index) => {
-    console.log("addRowImage");
     e.preventDefault();
+    setImageLoading(true);
     try {
       const id = index + 1;
-      const response = await fetch(`${ADDRESS}/6199269cde5f665787e70953/uploadImage`, {
+      const response = await fetch(`${ADDRESS}/619a8411ebb70416125daf8a/uploadImage`, {
         method: "POST",
         body: selectedImage,
       });
@@ -28,16 +30,13 @@ const ImageModal = ({ index }) => {
         console.log(data);
         setRowImage(data.images);
         console.log("Sucessfully posted");
+        setImageLoading(false);
       } else {
         console.log("File was not uploaded!");
       }
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const handleSubmit = (e) => {
-    console.log("submit");
   };
 
   const selectImage = (e) => {
@@ -60,7 +59,14 @@ const ImageModal = ({ index }) => {
           <Modal.Body>
             <div className="d-grid" id="selectimages">
               <div className="modalImageDiv d-flex justify-content-center ">
-                <Image className="" height="150" src={rowImage} />
+                {imageLoading ? (
+                  <div>
+                    <Spinner animation="border" variant="warning" size="sm" />
+                    <Spinner animation="border" variant="success" />
+                  </div>
+                ) : (
+                  <Image className="" height="150" src={rowImage} />
+                )}
               </div>
               <Form.Group className="mt-3">
                 <Form.Control id="picture" type="file" onChange={selectImage} />
