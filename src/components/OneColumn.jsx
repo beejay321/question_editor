efd49b1b9f619e5c1d9955dada9d68d267ed60c3
beejay321 <jibodu.busola@gmail.com>
2021-11-22 10:14:13 +0100

    Update OneColumn.jsx

--- src/components/OneColumn.jsx
+++ src/components/OneColumn.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageModal from "./ImageModal";
+import ContentEditable from "react-contenteditable";
 
 const OneColumn = ({ col, i, rowIndex }) => {
+  const [editableText, setEditableText] = useState(`Col ${i + 1}`);
+
   return (
     <>
       {col.type === "image" ? (
         <td className=" ">
           <div className="d-grid justify-content-start gap-2 ">
             <ImageModal />
-            <span className="d-flex justify-content-start">
-              <span contentEditable="true" className="rowText">
-                <em>Col {i + 1}</em>
-              </span>
-            </span>
+            <ContentEditable
+              className="rowText"
+              tagName="p"
+              html={editableText}
+              onChange={(e) => {
+                const html = e.target.value;
+                setEditableText(html);
+              }}
+            />
           </div>
         </td>
       ) : (
-        
-          <td className=" tableCell " >
-          <div className="d-grid justify-content-center pt-2  " >
+        <td className=" tableCell ">
+          <div className="d-grid justify-content-center pt-2  ">
             <input type={col.type} id={col.id} name={rowIndex} defaultValue={col.value} />
           </div>
           {/* <input type={col.type} id={col.id} name={i} defaultValue={col.value} /> //  if the selected buttons were to have the same name vertically*/}
