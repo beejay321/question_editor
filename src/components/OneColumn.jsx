92059b0b49cfc6c3ada8e5aef164aaaa209db92c
beejay321 <jibodu.busola@gmail.com>
2021-11-23 17:23:16 +0100

    Update OneColumn.jsx

--- src/components/OneColumn.jsx
+++ src/components/OneColumn.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ImageModal from "./ImageModal";
 import ContentEditable from "react-contenteditable";
 
@@ -25,31 +25,26 @@ const OneColumn = ({ col, i, rowIndex, key, arrOfColLabels }) => {
     console.log(inputValues);
   };
 
-
-
   return (
     <>
       {col.type === "image" ? (
-        <td key= {key} className=" tableCell ">
+        <td key={key} className=" tableCell ">
           <div className="d-grid justify-content-start gap-2 ">
-              <ImageModal imageClass="colImage" imageDivClass="colImageDiv" />  
-              <ContentEditable
+            <ImageModal imageClass="colImage" imageDivClass="colImageDiv" />
+            <ContentEditable
               className="rowText d-flex"
               tagName="p"
               html={colLabel}
               onChange={(e) => {
                 setColLabel(e.target.value);
-
               }}
-            />           
-          
-           
+            />
           </div>
         </td>
       ) : (
         <td key={key} className="tableCell">
           <div className="d-grid justify-content-start pt-2 px-3">
-            <input  type={inputValues.type} id={`${rowIndex}${i}`} name={`Row ${rowIndex}`} defaultValue={inputValues.value} onChange={(e) => handleChangeInput(i, e)} />
+            <input type={inputValues.type} id={`${rowIndex}${i}`} name={`Row ${rowIndex}`} defaultValue={inputValues.value} onChange={(e) => handleChangeInput(i, e)} />
           </div>
           {/* <input type={col.type} id={col.id} name={i} defaultValue={col.value} /> //  if the selected buttons were to have the same name vertically*/}
         </td>
