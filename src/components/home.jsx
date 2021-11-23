67730b541beeff2bb992b8e5412f5fcdfb6ec0eb
beejay321 <jibodu.busola@gmail.com>
2021-11-23 14:06:09 +0100

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,21 +6,21 @@ const Home = () => {
   return (
     <>
       <Container className=" ">
-        <div className="d-flex px-3">
+        <Row className="d-flex px-3">
           <Col sm={12} lg={6} className="d-grid justify-content-start px-3  pt-5  ">
               <h3 className="d-flex justify-content-start  pb-3">Question Edition View</h3>
               <EditionView />
           </Col>
           <Col className="d-flex justify-content-center " sm={0} lg={1}>
-            <div class="vertical"></div>
+            <div className="vertical"></div>
           </Col>{" "}
           <Col sm={12} lg={4} className="d-grid justify-content-start px-3 pt-5 ">
-            <div class="">
+            <div >
               <h3 className="d-flex justify-content-start  pb-3">Question Summary View</h3>
               <SummaryView />
             </div>
           </Col>
-        </div>
+        </Row>
       </Container>
     </>
   );
