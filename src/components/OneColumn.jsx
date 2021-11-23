1525f33aa3ec37b0773be182c422b2aaa21142fb
beejay321 <jibodu.busola@gmail.com>
2021-11-23 11:36:26 +0100

    Update OneColumn.jsx

--- src/components/OneColumn.jsx
+++ src/components/OneColumn.jsx
@@ -4,13 +4,29 @@ import ContentEditable from "react-contenteditable";
 
 const OneColumn = ({ col, i, rowIndex }) => {
   const [editableText, setEditableText] = useState(`Col ${i + 1}`);
+  const [inputValues, setInputValues] = useState({    
+    id: 0,
+    type: col.type,
+    name: "",
+    value: col.value
+  })  
+   
+
+  const handleChangeInput = (i, e) => {
+    setInputValues({ 
+    id: parseInt(e.target.id),
+    type: "radio",
+    name: e.target.name,
+    value: e.target.value})
+    console.log(inputValues)
+  };
 
   return (
     <>
       {col.type === "image" ? (
         <td className=" ">
           <div className="d-grid justify-content-start gap-2 ">
-            <ImageModal imageClass="colImage" imageDivClass ="colImageDiv" />
+            <ImageModal imageClass="colImage" imageDivClass="colImageDiv" />
             <ContentEditable
               className="rowText"
               tagName="p"
@@ -23,9 +39,9 @@ const OneColumn = ({ col, i, rowIndex }) => {
           </div>
         </td>
       ) : (
-        <td className=" tableCell ">
+        <td className="tableCell">
           <div className="d-grid justify-content-center pt-2  ">
-            <input type={col.type} id={col.id} name={rowIndex} defaultValue={col.value} />
+            <input type={inputValues.type} id={`${rowIndex}${i}`} name={`Row ${rowIndex}`} defaultValue={inputValues.value} onChange={(e) => handleChangeInput(i, e)} />
           </div>
           {/* <input type={col.type} id={col.id} name={i} defaultValue={col.value} /> //  if the selected buttons were to have the same name vertically*/}
         </td>
