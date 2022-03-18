c97e1b92711074b03b97390cf1a1d26da9b5929b
beejay321 <jibodu.busola@gmail.com>
2022-03-18 17:56:28 +0100

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,12 @@ const Home = () => {
   return (
     <>
       <Container className=" ">
-        <Row className="d-flex px-3">
+        <Row className="d-flex mt-5 px-3 ">
           <Col sm={12} lg={7} className="d-grid justify-content-start px-3  py-5 editorcol ">
             <h3 className="d-flex justify-content-start editortitle editorcol pb-3">Question Edition View</h3>
             <EditionView />
           </Col>
-          {/* <Col className="d-flex justify-content-center " sm={0} lg={1}>
-            <div className="vertical"></div>
-          </Col>{" "} */}
+
           <Col sm={12} lg={4} className="d-grid justify-content-start px-3  py-5 editorcol">
             <div className="">
               <h3 className="d-flex justify-content-start  editortitle editorcol pb-3 ">Question Summary View</h3>
